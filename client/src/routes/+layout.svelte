42fd4c7c5287cbdb67cd1d968532d900821953f4
glovek08 <gbrielbbarn@gmail.com>
2025-09-09 17:37:09 -0300

    feat: flowbite successfully installed and integrated

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
+	import "../app.css";
 	import favicon from '$lib/assets/favicon.svg';
 	import "the-new-css-reset/css/reset.css";
-	import "../styles/app.css"
-
+	import "../styles/app.css";
+	
 	let { children } = $props();
 </script>
 
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,15 @@
+import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [tailwindcss(), sveltekit()],
 	server: {
 		proxy: {
 			'/api': {
 				target: 'http://localhost/8000',
 				changeOrigin: true,
-				secure: false,
+				secure: false
 			}
 		}
 	}
