afce6eed88d36a153ae5592cb0b195078d2d39c7
glovek08 <gbrielbbarn@gmail.com>
2025-09-09 16:18:06 -0300

    refactor: move app config to init

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
-	import "../styles/app.css"
 	import "the-new-css-reset/css/reset.css";
+	import "../styles/app.css"
+
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
-
+<header></header>
 {@render children?.()}
 
 <style>
+
 </style>
